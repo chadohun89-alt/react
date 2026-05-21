47d60d60abd399a397d4743e90279fbf9b63c43f
chadohun89-alt <chadohun89@gmail.com>
2026-05-21 17:31:07 +0900

    5월 21일 수업 끝나고

--- frontend/src/page/GraphPage.jsx
+++ frontend/src/page/GraphPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function GraphPage(){
     const [ summary , setSummary] = useState(null);
@@ -7,28 +7,31 @@ function GraphPage(){
         const data = await res.json();
         setSummary(data);
     }
+
+    useEffect(() => {requestGraph();},[])
+
     return(
         <div>
             <h2>성적 & 출석 그래프</h2>
             <div className="flex gap-3">
                 <section>
                     <h2>성적 분포 그래프</h2>
-                    <img src="" />
+                    <img width="400" src="http://localhost:8000/analysis/score-graph" />
                     <div className="flex justify-between">
-                        <p>평균점수</p>
-                        <p>최고점수</p>
-                        <p>최저점수</p>
-                        <p>표준편차</p>
+                        <p>평균점수 {summary ? summary.avg_score :0}</p>
+                        <p>최고점수 {summary ? summary.max_score :0}</p>
+                        <p>최저점수{summary ? summary.min_score :0}</p>
+                        <p>표준편차{summary ? summary.std_score :0}</p>
                     </div>
                 </section>
                 <section>
                     <h2>출석률 분포 그래프</h2>
-                    <img src="" />
+                    <img width="400" src="http://localhost:8000/analysis/attend-graph" />
                     <div className="flex justify-between">
-                        <p>평균출석률</p>
-                        <p>최고출석률</p>
-                        <p>최저출석률</p>
-                        <p>표준편차</p>
+                        <p>평균출석률 {summary ?summary.avg_attend_rate :0}</p>
+                        <p>최고출석률 {summary ?summary.max_rate :0}</p>
+                        <p>최저출석률 {summary ? summary.min_rate :0}</p>
+                        <p>표준편차 {summary ? summary.std_rate :0}</p>
                     </div>
                 </section>
             </div>
